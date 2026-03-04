ac9563519fc8db166b6ef362c9d290e0b6d81e38
Future Artist Studio <futureartiststudio@gmail.com>
2026-03-04 20:34:16 +0000

    incearca sa repari eroarea asta te rog: Firebase Storage: No default buc

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:181793978792:web:02eaa5027af9baec0b459d",
   "apiKey": "AIzaSyB_BMJaZEyjcWmBnlx1x3L3RtYzsBbdpnU",
   "authDomain": "studio-4945159451-e7690.firebaseapp.com",
+  "storageBucket": "studio-4945159451-e7690.firebasestorage.app",
   "measurementId": "",
   "messagingSenderId": "181793978792"
 };
