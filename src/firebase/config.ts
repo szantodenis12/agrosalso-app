c94884d84460c5aa45dd8752c1103e619674284d
Future Artist Studio <futureartiststudio@gmail.com>
2026-03-04 18:56:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4945159451-e7690",
+  "appId": "1:181793978792:web:02eaa5027af9baec0b459d",
+  "apiKey": "AIzaSyB_BMJaZEyjcWmBnlx1x3L3RtYzsBbdpnU",
+  "authDomain": "studio-4945159451-e7690.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "181793978792"
+};
